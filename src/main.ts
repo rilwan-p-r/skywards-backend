815a1f7a97d3695898d8da4b5c8f35c310d0e09c
rilwan-p-r <rilwanpr@gmail.com>
2024-09-29 21:22:45 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.use(morgan('dev'));
   app.enableCors({
-    origin: 'http://localhost:5000',
+    origin: 'https://skyward-frontend.vercel.app/',
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
